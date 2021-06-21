45fe0422aa44ec838c25facf0c11b6e9295a9751
Joe Pitts <jpitts@truenorthit.co.uk>
2021-06-21 15:33:46 +0100

    Search region

--- src/app/input-form/input-form.component.ts
+++ src/app/input-form/input-form.component.ts
@@ -3,9 +3,7 @@ import { NgForm } from '@angular/forms';
 import {Data} from "./data-model" 
 
 import resourceJSON from "../../assets/resources.json";
-import { Observable } from 'rxjs';
-import {startWith, map} from 'rxjs/operators';
-import { group } from '@angular/animations';
+import regionsJSON from "../../assets/regions.json";
 
 
 
@@ -24,11 +22,16 @@ export class InputFormComponent implements OnInit {
   resources: any[];
   shownResources: any[];
 
+  regions: any[];
+  shownRegions: any[];
 
 
   constructor() {
     this.resources = resourceJSON;
     this.shownResources = this.resources;
+
+    this.regions = regionsJSON;
+    this.shownRegions = this.regions;
   }
 
   ngOnInit() {}
@@ -44,8 +47,8 @@ export class InputFormComponent implements OnInit {
   
 
     this.ngForm.valueChanges?.subscribe(form => {
-      // console.log(form)
-      this.search(form.resource)
+      if (form.resource) this.searchResource(form.resource);
+      if (form.region) this.searchRegion(form.region);
       this.formChange(form); 
     })      
 
@@ -55,22 +58,36 @@ export class InputFormComponent implements OnInit {
     this.modelEvent.emit(form);
   }
 
-  search(term: string){
+  searchResource(term: string){
     if (term == ""){
       this.shownResources = this.resources;
-      return
+      return;
     }
     this.shownResources= [];
     this.resources.forEach((group: {"name": string, "services": {"name": string, "short": string}[]}) => {
       let g: {"name": string, "services": {"name": string, "short": string}[]} = {"name": group.name, "services": []}
       group.services.forEach( (service: {"name": string, "short": string}) => {
-        if (service.name.toLowerCase().startsWith(term.toLowerCase())){
+        if (service.name.toLowerCase().includes(term.toLowerCase())){
           g.services.push(service);
         }
       });
       if(g.services.length != 0){
         this.shownResources.push(g)
       }
+    });
+  }
+
+  searchRegion(term: string){
+    if (term == ""){
+      this.shownRegions = this.regions;
+      return;
+    }
+    this.shownRegions= [];
+    this.regions.forEach((region: any) => {
+        if (region.name.toLowerCase().includes(term.toLowerCase())){
+          this.shownRegions.push(region);
+        }
+   
     });
   }
 

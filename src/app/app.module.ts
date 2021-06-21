b02976a2e638f5639285076add3c5b4b3b968f60
Joe Pitts <jpitts@truenorthit.co.uk>
2021-06-21 11:41:51 +0100

    Mockup done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select'; 
+import {MatInputModule} from '@angular/material/input'; 
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
+import { InputFormComponent } from './input-form/input-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MatToolbarModule, 
+    MatButtonModule,
+    MatSelectModule,
+    NgxMatSelectSearchModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

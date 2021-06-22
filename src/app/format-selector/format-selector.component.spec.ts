bfe20d5da4dbcfbd376f1f45456b812c48537104
Joe Pitts <jpitts@truenorthit.co.uk>
2021-06-22 11:02:05 +0100

    Tests for all components done.

--- src/app/format-selector/format-selector.component.spec.ts
+++ src/app/format-selector/format-selector.component.spec.ts
@@ -1,25 +1,60 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatCardActions, MatCardModule } from '@angular/material/card';
+import { Data } from '../input-form/data-model';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatButtonHarness } from '@angular/material/button/testing';
+import { HarnessLoader } from '@angular/cdk/testing';
+
+import { MatButtonModule } from '@angular/material/button';
 
 import { FormatSelectorComponent } from './format-selector.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 describe('FormatSelectorComponent', () => {
-  let component: FormatSelectorComponent;
   let fixture: ComponentFixture<FormatSelectorComponent>;
+  let loader: HarnessLoader;
+  let buttonHarness = MatButtonHarness;
+  const testData = new Data("T", "E", "S", "T", "!");
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FormatSelectorComponent ]
-    })
-    .compileComponents();
+      imports: [
+        MatButtonModule,
+        MatIconModule,
+        MatCardModule,
+        MatGridListModule
+      ],
+      declarations: [FormatSelectorComponent]
+    }).compileComponents();
+    fixture = TestBed.createComponent(FormatSelectorComponent);
+    fixture.componentInstance.data = testData;
+    fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
+
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FormatSelectorComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('should load all button harnesses', async () => {
+    const buttons = await loader.getAllHarnesses(MatButtonHarness);
+    expect(buttons.length).toBe(6);
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(fixture.componentInstance).toBeTruthy();
+  });
+
+  it("pressing a button should emmit an event", async () => {
+    const buttons = await loader.getAllHarnesses(MatButtonHarness);
+    spyOn(fixture.componentInstance.selectedChange, "emit");
+    await buttons[1].click();
+    expect(fixture.componentInstance.selectedChange.emit).toHaveBeenCalled()
+  });
+
+  it("pressing a button should change its apperance", async () => {
+    const buttons = await loader.getAllHarnesses(MatButtonHarness);
+    spyOn(fixture.componentInstance.selectedChange, "emit");
+    await buttons[1].click();
+    const hasColour = await(await (buttons[1].host())).hasClass("mat-accent");
+    expect(hasColour).toBe(true);
   });
 });
--- src/app/input-form/input-form.component.spec.ts
+++ src/app/input-form/input-form.component.spec.ts
@@ -1,25 +1,78 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Data } from '../input-form/data-model';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { HarnessLoader } from '@angular/cdk/testing';
 import { InputFormComponent } from './input-form.component';
+import { MatFormFieldHarness } from '@angular/material/form-field/testing';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputHarness } from '@angular/material/input/testing';
+
 
 describe('InputFormComponent', () => {
-  let component: InputFormComponent;
   let fixture: ComponentFixture<InputFormComponent>;
+  let loader: HarnessLoader;
+  const testData = new Data("T", "E", "prod", "T", "!");
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InputFormComponent ]
-    })
-    .compileComponents();
+      imports: [
+        MatFormFieldModule,
+        FormsModule,
+        MatAutocompleteModule,
+        MatSelectModule,
+        MatInputModule,
+        BrowserAnimationsModule
+      ],
+      declarations: [InputFormComponent]
+    }).compileComponents();
+    fixture = TestBed.createComponent(InputFormComponent);
+    fixture.componentInstance.model = testData;
+    fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
+
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(InputFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('should load all field harnesses', async () => {
+    const fields = await loader.getAllHarnesses(MatFormFieldHarness);
+    expect(fields.length).toBe(5);
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(fixture.componentInstance).toBeTruthy();
   });
+
+  it('should be able to get control of a form-field', async () => {
+    const formField = await loader.getAllHarnesses(MatFormFieldHarness);
+    expect(await formField[1].getControl() instanceof MatInputHarness).toBe(true);
+  });
+
+  it('changing a field should trigger an event with the correct data', async () => {
+    const fields = await loader.getAllHarnesses(MatFormFieldHarness);
+    const control = await fields[1].getControl() as MatInputHarness;
+
+    spyOn(fixture.componentInstance.modelEvent, "emit");
+    const testString = "My Test App"
+    await control.setValue(testString);
+
+    expect(fixture.componentInstance.modelEvent.emit).toHaveBeenCalledTimes(testString.length+1);
+
+    // @ts-ignore
+    let args = fixture.componentInstance.modelEvent.emit.calls.mostRecent().args;
+    expect(args[0]).toEqual(
+      { resource: testData.resource,
+        name: testString,
+        environment: testData.environment,
+        region: testData.region, 
+        instance: testData.instance }
+    );
+
+
+  });
+
+
 });
